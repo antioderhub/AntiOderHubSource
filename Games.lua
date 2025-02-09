5ebf0e20071c782331f51a3ba8ec3ef0d06d323f
aftr-lua <154597947+aftr-lua@users.noreply.github.com>
2025-02-09 22:08:35 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,4 +1,8 @@
 if game.PlaceId == 4924922222 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Brookhaven.lua"))()
 else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Universal.lua"))()
+end
+
+
   
