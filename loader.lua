89830df2fc60b6f17d47e5b430508aef1a440bb2
aftr-lua <154597947+aftr-lua@users.noreply.github.com>
2025-03-11 16:14:13 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 -- Loader was outdated i made a new
-setclipboard("https://antioderhub.fun/telegramchat")
+setclipboard("https://antioderhub.lol/telegramchat")
 wait()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Games.lua"))()
