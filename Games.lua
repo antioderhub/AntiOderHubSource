d1cc2b8e750e8116ca773088ee09f0a575932192
aftr-lua <154597947+aftr-lua@users.noreply.github.com>
2025-04-15 21:08:09 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 4924922222 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/3cd003438693662902381bf1e96b2becf5c0450c/Brookhaven.lua"))()
 elseif game.PlaceId == 9872472334 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Evade.lua"))()
+elseif game.PlaceId == 3101667897 then
+  loadstring(game:HttpGet("https://github.com/aftr-lua/AntiOderHub/raw/refs/heads/main/NewEraOfScripts/LegendsOfSpeed.lua"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Universal.lua"))()
 end
