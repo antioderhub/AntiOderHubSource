e4e0069777d33a0a3d484dbe8d8a4e91df56797c
end? <zhulinevgenij0@gmail.com>
2025-09-25 20:33:54 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,36 @@
--- Loader was outdated i made a new
-setclipboard("https://antioderhub.lol/telegramchat")
-wait()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aftr-lua/AntiOderHub/refs/heads/main/Games.lua"))()
+-- все дороги ведут в рим...
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.436135292, 0, 0.272917539, 0)
+ImageLabel.Size = UDim2.new(0, 297, 0, 283)
+ImageLabel.Image = "rbxassetid://130187469806463"
+
+TextLabel.Parent = ImageLabel
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0, 0, 1, 0)
+TextLabel.Size = UDim2.new(0, 297, 0, 241)
+TextLabel.FontFace = Font.new("rbxassetid://12187365364", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+TextLabel.Text = "AntiOder Hub is closed. Reason: RU AntiOdering killed. Wait a news in this script. Window close automatically in 20 seconds."
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 31.000
+TextLabel.TextWrapped = true
+TextLabel.TextYAlignment = Enum.TextYAlignment.Top
+
+wait(20)
+ScreenGui:Destroy()
